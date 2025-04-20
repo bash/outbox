8842c1cb352267605e0861ec97c3ebc308116c90
Tau Gärtli <git@tau.garden>
2025-04-21 01:33:18 +0200

    Fix URL

--- crates/outbox/src/lib.rs
+++ crates/outbox/src/lib.rs
@@ -19,7 +19,11 @@ impl Outbox {
 
     /// Adds a new message to the queue.
     pub async fn queue(&self, message: Vec<u8>) -> reqwest::Result<()> {
-        self.0.post("/queue").body(message).send().await?;
+        self.0
+            .post("http://localhost/queue")
+            .body(message)
+            .send()
+            .await?;
         Ok(())
     }
 }
